6b97044dd6bb7fe9eba073037e83e1471753438f
Sharif101 <sharif009009.com>
2023-09-10 18:08:57 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
-import Navbar from '../src/Components/Navbar/Navbar.jsx'
-import Financial from'../src/Components/Financial/Financial.jsx'
-import Retirement from '../src/Components/Retirement/Retirement.jsx'
+import Navbar from "../src/Components/Navbar/Navbar.jsx";
+import Financial from "../src/Components/Financial/Financial.jsx";
+import Retirement from "../src/Components/Retirement/Retirement.jsx";
+import Fetchdata from "./Components/Fetchdata/Fetchdata.jsx";
 
 function App() {
   return (
-    <div >
-      <Navbar></Navbar>
+    <div>
+      {/* <Navbar></Navbar>
       <Financial></Financial>
-      <Retirement></Retirement>
+      <Retirement></Retirement> */}
+      <Fetchdata></Fetchdata>
     </div>
   );
 }
